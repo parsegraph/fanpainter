75f54dd3191a9845deb59855f102655d649bae57
Aaron Faanes <dafrito@gmail.com>
2021-12-07 01:10:00 -0600

    Fix tests

--- test/test.ts
+++ test/test.ts
@@ -1,13 +1,13 @@
 import FanPainter from "../dist/parsegraph-fanpainter";
-import { GraphicsWindow } from "parsegraph-window";
 import Color from "parsegraph-color";
 
 import { mockDOM } from "node-canvas-webgl";
+import {BasicGLProvider} from "parsegraph-compileprogram";
 mockDOM(window);
 
 describe("Package", function () {
   it("works", () => {
-    const window = new GraphicsWindow();
+    const window = new BasicGLProvider("Provider", new Color(1, 1, 1, 1));
     const painter = new FanPainter(window);
     painter.selectDeg(0, 0, 0, 90, new Color(0, 0, 0, 1));
   });
