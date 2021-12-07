a72c4d05b56e76982642502590519825a6730c19
Aaron Faanes <dafrito@gmail.com>
2021-12-07 00:59:13 -0600

    Implement FanPainter; copied from parsegraph-node

--- test/test.ts
+++ test/test.ts
@@ -1,8 +1,14 @@
-import {assert} from "chai";
-import todo from "../dist/parsegraph-fanpainter";
+import FanPainter from "../dist/parsegraph-fanpainter";
+import { GraphicsWindow } from "parsegraph-window";
+import Color from "parsegraph-color";
+
+import { mockDOM } from "node-canvas-webgl";
+mockDOM(window);
 
 describe("Package", function () {
-  it("works", ()=>{
-    assert.equal(todo(), 42);
+  it("works", () => {
+    const window = new GraphicsWindow();
+    const painter = new FanPainter(window);
+    painter.selectDeg(0, 0, 0, 90, new Color(0, 0, 0, 1));
   });
 });
