ed65ee9ad0995e7d756f3c68d0edd857be0db750
Aaron Faanes <dafrito@gmail.com>
2022-02-26 22:43:29 -0600

    Fix API

--- test/test.ts
+++ test/test.ts
@@ -7,7 +7,7 @@ mockDOM(window);
 
 describe("Package", function () {
   it("works", () => {
-    const window = new BasicGLProvider("Provider", new Color(1, 1, 1, 1));
+    const window = new BasicGLProvider();
     const painter = new FanPainter(window);
     painter.selectDeg(0, 0, 0, 90, new Color(0, 0, 0, 1));
   });
