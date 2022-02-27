39670009ec212f457c4c95a87bed0c09323cb66a
Aaron Faanes <dafrito@gmail.com>
2022-02-26 23:03:29 -0600

    Show FanPainter in action

--- src/demo.ts
+++ src/demo.ts
@@ -8,36 +8,35 @@ document.addEventListener("DOMContentLoaded", () => {
   root.style.position = "relative";
   const glProvider = new BasicGLProvider();
 
-  const container = document.createElement("div");
-  container.appendChild(glProvider.container());
+  const container = glProvider.container();
   container.style.position = "absolute";
   container.style.left = "0px";
   container.style.top = "0px";
+  container.style.right = "0px";
+  container.style.bottom = "0px";
   container.style.pointerEvents = "none";
   root.appendChild(container);
   container.style.fontSize = "18px";
   container.style.fontFamily = "sans";
   const refresh = () => {
     const painter = new FanPainter(glProvider);
+    painter.setSelectionSize(Math.PI);
+    painter.setSelectionAngle(Math.random() * Math.PI * 2);
+    painter.selectDeg(
+      Math.random() * glProvider.width(),
+      Math.random() * glProvider.height(),
+      0,
+      0,
+      Color.random()
+    );
     const animate = () => {
+      glProvider.gl().viewport(0, 0, glProvider.width(), glProvider.height());
       const mat = make2DProjection(glProvider.width(), glProvider.height());
-      painter.selectDeg(
-        Math.random() * glProvider.width(),
-        Math.random() * glProvider.height(),
-        Math.random() * 360,
-        Math.random() * 360,
-        Color.random()
-      );
       glProvider.render();
       painter.render(mat);
       requestAnimationFrame(animate);
     };
     requestAnimationFrame(animate);
-    const rand = () => Math.floor(Math.random() * 255);
-    document.body.style.backgroundColor = `rgb(${rand()}, ${rand()}, ${rand()})`;
-    container.style.color = `rgb(${rand()}, ${rand()}, ${rand()})`;
-    container.style.left = `${Math.random() * root.clientWidth}px`;
-    container.style.top = `${Math.random() * root.clientHeight}px`;
   };
 
   const dot = document.createElement("div");
